67fb6707433b61787c5685fcfd91913bfa7d3e27
Badi-Haki Martin <jojackblack@gmail.com>
2022-03-26 12:32:36 -0400

    commit tree b620bc13a02cedbd5293694247915e92041ace90

--- index.js
+++ index.js
@@ -1,5 +1,13 @@
 function isPalindrome(word) {
   // Write your algorithm here
+  // this funciton takes in a word and finds out if it's a palindrome
+  // apparently a palindrome is a word that's the same front and back
+  const reserveWord = [];
+  for (const letter of word){
+    reserveWord.unshift(letter);
+  }
+  console.log(reserveWord.join(''));
+  return reserveWord.join('') === word;
 }
 
 /* 
@@ -8,6 +16,10 @@ function isPalindrome(word) {
 
 /*
   Add written explanation of your solution here
+  Ok, so the isPalindrome function needs to take a word and see if it's the same front and back
+  -SO I need to establish an ARRAY variable, 'reverseWord', to compare the original 'word' to
+  -After that, I need to iterate over the length of the 'word' and UNSHIFT each letter to the front of the array
+  -Finally, I need to turn 'reserseWord' into a string. String it up, and return word === reverseWord
 */
 
 // You can run `node index.js` to view these console logs
@@ -16,7 +28,8 @@ if (require.main === module) {
   console.log("Expecting: true");
   console.log("=>", isPalindrome("racecar"));
 
-  console.log("");
+  console.log("Expecting: True");
+  console.log(`=>`, isPalindrome('mom'))
 
   console.log("Expecting: false");
   console.log("=>", isPalindrome("robot"));
